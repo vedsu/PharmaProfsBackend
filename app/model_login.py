1f4b510fed7d5a5a2728809e1ac68f2f63a4dcb2
vedsu <90701239+vedsu@users.noreply.github.com>
2025-03-17 12:00:20 +0530

    Update model_login.py

--- app/model_login.py
+++ app/model_login.py
@@ -1,5 +1,3 @@
-# Login and Registration Component
-
 from app import mongo
 from app import mail
 from flask_mail import Message
@@ -8,25 +6,20 @@ from flask import render_template_string
 class Login():
 
     @staticmethod
-    def register(register_email, register_password, register_confirmpassword, register_type, website):
+    def register(register_name,register_email,register_role,register_number,register_password,register_type,website):
         
-        # Create WebsiteUrl for respective Websites
-        if website =="PHARMAPROFS":
-            websiteUrl = "https://pharmaprofs.com/"
-        else: 
-            websiteUrl = " "
+        websiteUrl = "https://pharmaprofs.com/"
         
-        if register_password == register_confirmpassword:
-
-            user = mongo.db.user_data.find_one({"email": register_email})
-            if user:
-                return ({"success": False, "message": "User already registered, Please Login"}),403
-            else:
-                try:
-                    msg = Message('Your Account Credentials', sender = 'registration@pharmaprofs.com', recipients = [register_email])
+        user = mongo.db.user_data.find_one({"email":register_email})
+        
+        if user:
+            return ({"success":False, "message":"User already registered, Please Login"}),203
+        else:
+            try:
+                msg = Message('Your Account Credentials', sender = 'registration@pharmaprofs.com', recipients = [register_email])
                     
-                    msg.body = f"""
-                                   Dear Customer,
+                msg.body = f"""
+                                   Dear Customer {register_name},
 
                                    Welcome to our website!
 
@@ -42,8 +35,8 @@ class Login():
                                    Webinar Organizer Team
                                    
                                    """
-                    msg.html = render_template_string("""
-                                   <p>Dear Customer,</p>
+                msg.html = render_template_string("""
+                                   <p>Dear Customer {{name}},</p>
                                    <p>Welcome to our website!</p>
                                    <p>Here are your account credentials:</p>
                                    <ul>
@@ -53,47 +46,50 @@ class Login():
                                    </ul>
                                    <p>Please keep this information secure and do not share it with anyone.</p>
                                    <p>Thanks & Regards!<br>Webinar Organizer Team</p>
-                                   """, email=register_email, password=register_password, website=websiteUrl)
-                    mail.send(msg)
+                                   """, name = register_name,email=register_email, password=register_password, website=websiteUrl)
+                mail.send(msg)
+                user = {"name":register_name,"role":register_role,"email":register_email,"contact":register_number}
+                if register_type == "Attendee":
                     
-                    if register_type == "Attendee":
-                        try:
-                            mongo.db.user_data.insert_one({"email":register_email, "password":register_password, "UserType": register_type, "website":website,"history_purchased":[], "history_pending":[]})
-                            return ({"success": True, "message": "user registered successfully, email sent"}),201
-                        except Exception as e:
-                            return ({"success": False, "message": str(e)}),403
-
-                    else:
+                    try:
                         
-                        try:
-                            mongo.db.user_data.insert_one({"email":register_email, "password":register_password, "UserType": register_type, "website":website})
-                            return ({"success": True, "message": "user registered successfully, email sent"}),201
-                        except Exception as e:
-                            return ({"success": False, "message": str(e)}),403
+                        mongo.db.user_data.insert_one({"name":register_name,"role":register_role,"email":register_email,"contact":register_number, "password":register_password,
+                                                        "UserType": register_type, "website":website,
+                                                        "history_purchased":[], "history_pending":[],"newsletter_purchased":[], "newsletter_pending":[] })
+                        return  ({"success": True, "message":user}),201
+                    except Exception as e:
+                        return ({"success":False, "message":"Error in registering user"}),203
+                else:
+                    
+                    try:
+                            mongo.db.user_data.insert_one({"name":register_name,"role":register_role,"email":register_email,"contact":register_number, "password":register_password, "UserType": register_type, "website":website})
+                            return ({"success": True, "message": user}),201
+                    except Exception as e:
+                            return ({"success": False, "message": str(e)}),203
 
-                except Exception as e:
-                    return ({"success": False, "message": str(e)}),403
-        else:
-            return ({"success": False, "message": "password mismatch"}),403
-
-
+            except Exception as e:
+                return({"success":False, "message":str(e)}),203
+    
     @staticmethod
     def authenticate(login_email, login_password, login_type, website):
 
         try:
-            user = mongo.db.user_data.find_one({"email":login_email, "password": login_password, "UserType": login_type,"website":website})
+            # user = mongo.db.user_data.find_one({"email":login_email, "password": login_password, "UserType": login_type,"website":website})
+            
+            user = mongo.db.user_data.find_one({"email":login_email, "password": login_password, "UserType": login_type,"website":website},{"_id":0})
             if user:
-                return ({"success": True, "message": "login successfull"}),200
+                # return ({"success": True, "message": "login successfull"}),200
+                return ({"success": True, "message": user}),200
             else:
-                return ({"success": False, "message": "invalid credentials"}),403
+                return ({"success": False, "message": "invalid credentials"}),203
             
         except Exception as e:
-            return ({"success": False, "message":str(e)}),403      
-        
-
+            return ({"success": False, "message":str(e)}),203          
+    
+    
     @staticmethod
     def user_order(user, order_type, webinar):
-        
+            
         try:
             if order_type == "purchased":
                 mongo.db.user_data.update_one(
@@ -107,5 +103,28 @@ class Login():
                 )
 
             return ({"success": True, "message":"webinar updated for user"}),200
+        
         except Exception as e:
-            return ({"success": False, "message":str(e)}),403
+            return ({"success": False, "message":str(e)}),203
+        
+    
+    @staticmethod
+    def user_newsletterorder(user, order_type, webinar):
+            
+        try:
+            if order_type == "purchased":
+                mongo.db.user_data.update_one(
+                {"email":user},
+                {"$addToSet":{"newsletter_purchased":webinar}}
+                )
+            else:
+                mongo.db.user_data.update_one(
+                {"email":user},
+                {"$addToSet":{"newsletter_pending":webinar}}
+                )
+
+            return ({"success": True, "message":"newsletter updated for user"}),200
+        
+        except Exception as e:
+        
+            return ({"success": False, "message":str(e)}),203
