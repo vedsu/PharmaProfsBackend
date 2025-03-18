09cb5e35c7caff796c6c1b5362cd17e6f416ca89
vedsu <90701239+vedsu@users.noreply.github.com>
2025-03-18 10:47:59 +0530

    Update model_utility.py

--- app/model_utility.py
+++ app/model_utility.py
@@ -12,6 +12,28 @@ from io import BytesIO
 
 
 class Utility():
+    
+    @staticmethod
+    def view_coupon():
+        coupon_list = [] 
+        try:
+            coupon_data = list(mongo.db.coupon_data.find({}))
+            for coupon in coupon_data:
+                
+                    coupon_dict = {
+                    "id": coupon.get("id"),
+                    "coupon": coupon.get("coupon"),
+                    "type": coupon.get("type"),
+                    "amount": coupon.get("amount"),
+                    "status": coupon.get("status")
+                    }
+                    coupon_list.append(coupon_dict)
+        except Exception as e:
+            coupon_list = {"error": str(e)}           
+        
+        return coupon_list
+    
+    
     @staticmethod
     def update_live_status():
         # Get current time in EST
@@ -32,107 +54,288 @@ class Utility():
         except Exception as e:
             return {"success":False, "message":str(e)}
 
-    
-    
     @staticmethod
-    def generate_pdf(Webinar,customername, country, websiteUrl, customeremail, date_time_str, webinardate, comma_separated_keys, orderamount, invoice_number):
-        # File and document details
-        documentTitle = 'Payment Receipt'
-        title = 'Invoice Details'
-        
-        # Sections of the Invoice
+    def generate_pdf(Webinar, customername, country, websiteUrl, billingemail, date_time_str, orderamount, invoice_number, discount, zip_code, orderID):
+        total_price = int(orderamount) + int(discount)
+        def wrap_text(text, max_chars_per_line):
+            words = text.split()
+            lines = []
+            current_line = ""
+            
+            for word in words:
+                if len(current_line) + len(word) + 1 <= max_chars_per_line:
+                    current_line += " " + word if current_line else word
+                else:
+                    lines.append(current_line)
+                    current_line = word
+            
+            if current_line:
+                lines.append(current_line)
+            
+            return lines
+        documentTitle = "PharmaProfs"
+        title = "Invoice Details"
+    
         leftSection = [
-            f'Invoice Number - {invoice_number}',
-            f'Order Date - {date_time_str}'
+        f'Invoice Number # {invoice_number}',
+        f'Date : {date_time_str}',
         ]
-        rightSection = [
-            f'Order Amount - {orderamount}'
+    
+        rightSection1 = [
+        f'support@pharmprofs.com',
+        f'https://pharmaprofs.com/'
         ]
+    
         customerDetails = [
-            'Customer Details:',
-            f'Customer Name -  {customername}',
-            f'Registered Email - {customeremail}',
-            f'Country - {country}',
-            # f'Webinar Date: {webinardate}',
-            # f'Webinar Session: {comma_separated_keys}'
+        'Recipient Details:',
+        f'Name : {customername}',
+        f'Email : {billingemail}',
+        f'Country : {country}',
+        f'Zip Code : {zip_code}'
         ]
+    
+        orderDetails = [
+        f'#{orderID}'
+        ]
+    
         webinarDetails = [
-            'Webinar Name -',
-            f'{Webinar}'  # Line break handled by keeping as separate lines
+        'Description'
         ]
-        thankYouNote = 'query?reach out to us at support@pharmprofs.com !'
-        signature = 'Webinar Organizer Team'
-
-        # Create PDF in memory
+        max_chars_per_line = 60
+        wrapped_webinar_details = wrap_text(Webinar, max_chars_per_line)
+        
         buffer = BytesIO()
         pdf = canvas.Canvas(buffer, pagesize=letter)
         width, height = letter
 
-        # Set the title of the document
-        pdf.setTitle(documentTitle)
-
-        # Add border around the page
         pdf.rect(20, 20, width - 40, height - 40, stroke=1, fill=0)
+        pdf.setFont("Helvetica-Bold", 79)
+        pdf.setFillColorRGB(0.9, 0.9, 0.9)
+        pdf.saveState()
+        pdf.translate(width / 2, height / 2)
+        pdf.rotate(45)
+        pdf.drawCentredString(0, 0, "HCProfs")
+        pdf.restoreState()
+        pdf.setFillColor(colors.black)
 
-        # Shift all content downward
-        y_shift = 40  # Shift content down by 40 units
+        y_shift = 50
 
-        # Document Title at the top
-        pdf.setFont('Helvetica-Bold', 20)
+        pdf.setFont('Helvetica-Bold', 17)
         pdf.drawCentredString(width / 2, height - 40 - y_shift, documentTitle)
+        pdf.setFont('Helvetica-Bold', 15)
+        pdf.drawCentredString(width / 2, height - 75 - y_shift, title)
 
-        # Create the title by setting its font and putting it on the canvas
-        pdf.setFont('Helvetica-Bold', 16)
-        pdf.drawCentredString(width / 2, height - 80 - y_shift, title)
-
-        # First section - Invoice details
-        pdf.setFont("Helvetica-Bold", 14)
-        text = pdf.beginText(40, height - 120 - y_shift)
+        pdf.setFont("Helvetica-Bold", 11)
+        text = pdf.beginText(40, height - 105 - y_shift)
         for line in leftSection:
             text.textLine(line)
         pdf.drawText(text)
 
-        text = pdf.beginText(width - 250, height - 120 - y_shift)
-        for line in rightSection:
+        text = pdf.beginText(width - 180, height - 105 - y_shift)
+        for line in rightSection1:
             text.textLine(line)
         pdf.drawText(text)
 
-        # Line separator for the second section
-        pdf.line(40, height - 160 - y_shift, width - 40, height - 160 - y_shift)
+        pdf.line(40, height - 170 - y_shift, width - 40, height - 170 - y_shift)
 
-        # Second section - Customer details
-        text = pdf.beginText(40, height - 180 - y_shift)
-        text.setFont("Helvetica-Bold", 14)
-        text.textLine(customerDetails[0])  # Customer Details heading
-        text.setFont("Helvetica", 14)  # Change font for the rest of the text
-        text.moveCursor(0, 20)  # Add space after the heading
-
+        text = pdf.beginText(40, height - 190 - y_shift)
+        text.setFont("Helvetica-Bold", 13)
+        text.textLine(customerDetails[0])
+        text.setFont("Helvetica", 11)
+        text.moveCursor(0, 18)
         for line in customerDetails[1:]:
             text.textLine(line)
-            text.moveCursor(0, 15)  # Add space between lines to avoid overwriting
         pdf.drawText(text)
 
-        # Line separator for the third section
-        pdf.line(40, height - 340 - y_shift, width - 40, height - 340 - y_shift)
-
-        # Third section - Webinar details
-        text = pdf.beginText(40, height - 360 - y_shift)
-        text.setFont("Helvetica-Bold", 14)
-        for line in webinarDetails:
+        text = pdf.beginText(width - 180, height - 190 - y_shift)
+        for line in orderDetails:
             text.textLine(line)
         pdf.drawText(text)
 
-        # Add the thank you note and signature at the bottom of the page
-        pdf.setFont("Helvetica-Oblique", 12)
-        pdf.setFillColor(colors.black)
-        pdf.drawCentredString(width / 2, 80, thankYouNote)
-        pdf.drawCentredString(width / 2, 60, signature)
         
-        # Add the website URL at the bottom-most position
-        pdf.setFont("Helvetica", 12)
-        pdf.drawCentredString(width / 2, 40, f'Website - {websiteUrl}')
+        pdf.line(40, height - 320 - y_shift, width - 40, height - 320 - y_shift)
 
-        # Save the PDF to the in-memory buffer
+        text = pdf.beginText(40, height - 340 - y_shift)
+        text.setFont("Helvetica-Bold", 11)
+        text.textLine(webinarDetails[0])
+        for line in wrapped_webinar_details:
+            text.textLine(line)
+        pdf.drawText(text)
+
+        text = pdf.beginText(width - 180, height - 340 - y_shift)
+        text.setFont("Helvetica-Bold", 11)
+        text.textLine('Total Price')
+        text.textLine(f'${total_price}')
+        pdf.drawText(text)
+
+        text = pdf.beginText(width - 180, height - 420 - y_shift)
+        text.setFont("Helvetica-Bold", 11)
+        text.textLine(f'Subtotal: ${int(total_price)}')
+        text.textLine(f'Discount: -${int(discount)}')
+        text.textLine(f'Grand Total: ${int(orderamount)}')
+        pdf.drawText(text)
+
+        thankYouNote = 'Thank you for your Purchase'
+        queryNote = 'Query? Reach out to us at support@pharmprofs.com !'
+        signature = 'Webinar Organizer Team'
+        pdf.setFont("Helvetica-Oblique", 11)
+        pdf.setFillColor(colors.black)
+        pdf.drawCentredString(width / 2, 125, thankYouNote)
+        pdf.drawCentredString(width / 2, 105, queryNote)
+        pdf.drawCentredString(width / 2, 85, signature)
+        
+        pdf.setFont("Helvetica", 11)
+        pdf.drawCentredString(width / 2, 65, f'Website - {websiteUrl}')
+        
+        pdf.save()
+        buffer.seek(0)
+
+        # Upload the PDF to S3
+        bucket_name = "webinarprofs"
+        object_key = f'websiteorderist/{invoice_number}.pdf'
+        s3_client.put_object(
+            Body=buffer,
+            Bucket=bucket_name,
+            Key=object_key
+        )
+
+        # Generate S3 URL
+        s3_url = f"https://{bucket_name}.s3.amazonaws.com/{object_key}"
+        return s3_url
+    
+    @staticmethod
+    def generatelocal_pdf(Webinar, customername, country, websiteUrl, billingemail, date_time_str, orderamount, invoice_number, discount, zip_code, orderID):
+        total_price = int(orderamount) + int(discount)
+        def wrap_text(text, max_chars_per_line):
+            words = text.split()
+            lines = []
+            current_line = ""
+            
+            for word in words:
+                if len(current_line) + len(word) + 1 <= max_chars_per_line:
+                    current_line += " " + word if current_line else word
+                else:
+                    lines.append(current_line)
+                    current_line = word
+            
+            if current_line:
+                lines.append(current_line)
+            
+            return lines
+        documentTitle = "PharmaProfs"
+        title = "Invoice Details"
+    
+        leftSection = [
+        f'Invoice Number # {invoice_number}',
+        f'Date : {date_time_str}',
+        ]
+    
+        rightSection1 = [
+        f'support@pharmprofs.com',
+        f'https://pharmaprofs.com/'
+        ]
+    
+        customerDetails = [
+        'Recipient Details:',
+        f'Name : {customername}',
+        f'Email : {billingemail}',
+        f'Country : {country}',
+        f'Zip Code : {zip_code}'
+        ]
+    
+        orderDetails = [
+        f'#{orderID}'
+        ]
+    
+        webinarDetails = [
+        'Description'
+        ]
+        max_chars_per_line = 60
+        wrapped_webinar_details = wrap_text(Webinar, max_chars_per_line)
+        
+        buffer = BytesIO()
+        pdf = canvas.Canvas(buffer, pagesize=letter)
+        width, height = letter
+
+        pdf.rect(20, 20, width - 40, height - 40, stroke=1, fill=0)
+        pdf.setFont("Helvetica-Bold", 79)
+        pdf.setFillColorRGB(0.9, 0.9, 0.9)
+        pdf.saveState()
+        pdf.translate(width / 2, height / 2)
+        pdf.rotate(45)
+        pdf.drawCentredString(0, 0, "HCProfs")
+        pdf.restoreState()
+        pdf.setFillColor(colors.black)
+
+        y_shift = 50
+
+        pdf.setFont('Helvetica-Bold', 17)
+        pdf.drawCentredString(width / 2, height - 40 - y_shift, documentTitle)
+        pdf.setFont('Helvetica-Bold', 15)
+        pdf.drawCentredString(width / 2, height - 75 - y_shift, title)
+
+        pdf.setFont("Helvetica-Bold", 11)
+        text = pdf.beginText(40, height - 105 - y_shift)
+        for line in leftSection:
+            text.textLine(line)
+        pdf.drawText(text)
+
+        text = pdf.beginText(width - 180, height - 105 - y_shift)
+        for line in rightSection1:
+            text.textLine(line)
+        pdf.drawText(text)
+
+        pdf.line(40, height - 170 - y_shift, width - 40, height - 170 - y_shift)
+
+        text = pdf.beginText(40, height - 190 - y_shift)
+        text.setFont("Helvetica-Bold", 13)
+        text.textLine(customerDetails[0])
+        text.setFont("Helvetica", 11)
+        text.moveCursor(0, 18)
+        for line in customerDetails[1:]:
+            text.textLine(line)
+        pdf.drawText(text)
+
+        text = pdf.beginText(width - 180, height - 190 - y_shift)
+        for line in orderDetails:
+            text.textLine(line)
+        pdf.drawText(text)
+
+        
+        pdf.line(40, height - 320 - y_shift, width - 40, height - 320 - y_shift)
+
+        text = pdf.beginText(40, height - 340 - y_shift)
+        text.setFont("Helvetica-Bold", 11)
+        text.textLine(webinarDetails[0])
+        for line in wrapped_webinar_details:
+            text.textLine(line)
+        pdf.drawText(text)
+
+        text = pdf.beginText(width - 180, height - 340 - y_shift)
+        text.setFont("Helvetica-Bold", 11)
+        text.textLine('Total Price')
+        text.textLine(f'${total_price}')
+        pdf.drawText(text)
+
+        text = pdf.beginText(width - 180, height - 420 - y_shift)
+        text.setFont("Helvetica-Bold", 11)
+        text.textLine(f'Subtotal: ${int(total_price)}')
+        text.textLine(f'Discount: -${int(discount)}')
+        text.textLine(f'Grand Total: ${int(orderamount)}')
+        pdf.drawText(text)
+
+        thankYouNote = 'Thank you for your Purchase'
+        queryNote = 'Query? Reach out to us at support@pharmprofs.com !'
+        signature = 'Webinar Organizer Team'
+        pdf.setFont("Helvetica-Oblique", 11)
+        pdf.setFillColor(colors.black)
+        pdf.drawCentredString(width / 2, 125, thankYouNote)
+        pdf.drawCentredString(width / 2, 105, queryNote)
+        pdf.drawCentredString(width / 2, 85, signature)
+        
+        pdf.setFont("Helvetica", 11)
+        pdf.drawCentredString(width / 2, 65, f'Website - {websiteUrl}')
+        
         pdf.save()
         buffer.seek(0)
 
@@ -148,6 +351,8 @@ class Utility():
         # Generate S3 URL
         s3_url = f"https://{bucket_name}.s3.amazonaws.com/{object_key}"
         return s3_url
+
+    
     
     @staticmethod
     def subscribe_list(subscriber_email, subscriber_name, subscription_type, subscriber_jobtitle):
@@ -168,17 +373,17 @@ class Utility():
             return ({"success": False, "message": str(e)}), 403
        
     @staticmethod
-    def forgotpassword(email, website):
+    def forgotpassword(email, website = "PHARMAPROFS"):
         try:
             usercredentails =list(mongo.db.user_data.find({"$and":[{"email":email},{"website":website}]}))
             if usercredentails:
-               try:
+                try:
                     usercredentail = usercredentails[0]
                     email = usercredentail.get("email")
                     password = usercredentail.get("password")
                     websiteUrl = usercredentail.get("websiteUrl")
                     
-                    msg = Message('Your Account Credentials', sender = 'registration@pharmaprofs.com', recipients = [email])
+                    msg = Message('Your Account Credentials', sender = 'cs@hcprofs.com', recipients = [email])
                     
                     msg.body = f"""
                                    Dear Customer,
@@ -211,7 +416,8 @@ class Utility():
                                    """, email=email, password=password, website=websiteUrl)
                     mail.send(msg)
                     return ({"success": True, "message": "email sent successfully"}),200
-               except Exception as e:
+                
+                except Exception as e:
                    return ({"success": False, "message": str(e)}), 403
 
             else:
