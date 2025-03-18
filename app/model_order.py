29a40c4e6fb02a37c100e748636a81711ca821ab
vedsu <90701239+vedsu@users.noreply.github.com>
2025-03-18 10:34:47 +0530

    Update model_order.py

--- app/model_order.py
+++ app/model_order.py
@@ -2,8 +2,18 @@
 from app import mongo
 from datetime import datetime, timedelta
 import pytz
+
 class Order():
 
+    @staticmethod
+    def update_corporateorder(order_data):
+
+        try:
+            mongo.db.corporate_order.insert_one(order_data)
+            return ({"success": True, "message": "order placed successfully"}),201
+        except Exception as e:
+            return ({"success": False, "message": str(e)}), 403
+    
     @staticmethod
     def update_order(order_data):
 
@@ -12,7 +22,67 @@ class Order():
             return ({"success": True, "message": "order placed successfully"}),201
         except Exception as e:
             return ({"success": False, "message": str(e)}), 403
-        
+    @staticmethod
+    def update_newsletterorder(order_data):
+
+        try:
+            mongo.db.newsletter_order.insert_one(order_data)
+            return ({"success": True, "message": "order placed successfully"}),201
+        except Exception as e:
+            return ({"success": False, "message": str(e)}), 403
+    @staticmethod
+    def find_newsletterorder(customeremail):
+    
+        dashboard_list =[]
+        try:
+            orders = list(mongo.db.newsletter_order.find({"customeremail":customeremail}))
+            user_data = list(mongo.db.user_data.find({"email": customeremail}))
+            if user_data:
+                user = user_data[0]
+                newsletter_purchased = user.get("newsletter_purchased") 
+                newsletter_pending = user.get("newsletter_pending")
+                
+                
+                for order in orders:
+                    
+                    o_id = order.get("id")
+                    topic = order.get("topic")
+                    customeremail = order.get("customeremail")
+                    paymentstatus = order.get("paymentstatus")
+                    customername = order.get("customername")
+                    document = order.get("document")
+
+                    if paymentstatus == "purchased":
+                        projection ={"_id":0}
+                        newsletter_data  = list(mongo.db.newsletter_data.find({"topic":topic}, projection))
+                        if newsletter_data:
+                            newsletter = newsletter_data[0]
+                            # print("YES")
+                            w_id = newsletter.get("id")
+                            topic = newsletter.get("topic"),
+                            published_date = newsletter.get("published_date"),
+                            newsletter_doc = newsletter.get("document")
+                            
+                            
+                            
+                            dashboard_dict = {
+                            "o_id":o_id,
+                            "w_id":w_id,
+                            "newsletter" : topic[0],
+                            "document" : document,
+                            "published_date":published_date[0],
+                            "newsletter_doc":newsletter_doc    
+                            }
+
+                            dashboard_list.append(dashboard_dict)
+
+                        
+        except Exception as e:
+
+                dashboard_list=[str(e)]
+
+        return dashboard_list, newsletter_purchased, newsletter_pending
+    @staticmethod
     def find_order(customeremail):
 
         dashboard_list =[]
@@ -28,6 +98,7 @@ class Order():
                 for order in orders:
                     
                     live_url, recording_url, digitaldownload_url, transcript_url = None, None, None, None
+                    o_id = order.get("id")
                     topic = order.get("topic")
                     customeremail = order.get("customeremail")
                     paymentstatus = order.get("paymentstatus")
@@ -37,6 +108,8 @@ class Order():
                     sessionTranscript = order.get("sessionTranscript") # True or False
                     customername = order.get("customername")
                     document = order.get("document")
+                    order_type = order.get("order_type")
+                    total_attendee = order.get("total_attendee")
 
                     if paymentstatus == "purchased":
                         projection ={"_id":0}
@@ -44,6 +117,7 @@ class Order():
                         if webinar_data:
                             webinar = webinar_data[0]
                             # print("YES")
+                            w_id = webinar.get("id")
                             date = webinar.get("date")
                             time = webinar.get("time")
                             topic = webinar.get("topic")
@@ -58,16 +132,21 @@ class Order():
                             handle_live = handle_timezone(date_time, timeZone)
                             handle_other = handle_othertimezone(date_time, timeZone)
                             
-                            if sessionLive == "True" and handle_live:
+                            if sessionLive == "true" and handle_live:
                                 live_url =  urlLive
-                            if sessionRecording == "True" and handle_other:
+                            if sessionRecording == "true":
+                                # if sessionRecording == "true" and handle_other:
                                 recording_url = urlRecording
-                            if sessionDigitalDownload == "True" and handle_other:
+                            if sessionDigitalDownload == "true":
+                                # if sessionDigitalDownload == "true" and handle_other:
                                 digitaldownload_url = urlDigitalDownload
-                            if sessionTranscript == "True" and handle_other:
+                            if sessionTranscript == "true":
+                            # if sessionTranscript == "true" and handle_other:
                                 transcript_url = urlTranscript
                             
                             dashboard_dict = {
+                            "o_id":o_id,
+                            "w_id":w_id,
                             "customername":customername ,
                             "webinar" : topic,
                             "speaker" : speaker ,
@@ -79,7 +158,9 @@ class Order():
                             "recording_url": recording_url,
                             "digitaldownload_url": digitaldownload_url,
                             "transcript_url" : transcript_url,
-                            "document" : document
+                            "document" : document,
+                            "order_type":order_type,
+                            "total_attendee":total_attendee
                             }
 
                             dashboard_list.append(dashboard_dict)
@@ -126,7 +207,7 @@ def handle_othertimezone(webinar_datetime_str,timeZone):
         time_difference = webinar_datetime_utc - current_datetime_utc
 
         # Check if the webinar is within the next 24 hours
-        is_more_than_24_hours = timedelta(24) < time_difference < timedelta(hours=1440)
+        is_more_than_24_hours = timedelta(hours=24) < time_difference < timedelta(hours=1440)
 
         return is_more_than_24_hours
           
@@ -168,6 +249,6 @@ def handle_timezone(webinar_datetime_str,timeZone):
         time_difference = webinar_datetime_utc - current_datetime_utc
 
         # Check if the webinar is within the next 24 hours
-        is_within_next_24_hours = timedelta(0) < time_difference < timedelta(hours=24)
+        is_within_next_24_hours = timedelta(hours=0) < time_difference < timedelta(hours=24)
 
         return is_within_next_24_hours
